b0edb0c6ed9e77a667df268fc0739bd397026d2f
NikolasSilvaLunkes <111617750+NikolasSilvaLunkes@users.noreply.github.com>
2022-11-20 17:28:31 -0300

    your comment

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'whfxmu35o6%2)9z71og9*efq^7++so1%@i-nwekkj2d8&fi=$t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1]
+ALLOWED_HOSTS = ['http://127.0.0.1','localhost','0.0.0.0','*']
 
 
 # Application definition
@@ -77,16 +77,23 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'personalsite',
-      'USER':'postgres',
-      'PASSWORD':'0606',
-      'HOST':'localhost',
-      'PORT':'5432',
-   }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES={
+#   'default':{
+#      'ENGINE':'django.db.backends.postgresql_psycopg2',
+#      'NAME':'personalsite',
+#      'USER':'postgres',
+#     'PASSWORD':'0606',
+#      'HOST':'localhost',
+#      'PORT':'5432',
+#   }
+#}
 
 
 # Password validation
